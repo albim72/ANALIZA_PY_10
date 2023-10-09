721db6421488198ca1b44b8fad9bec2c911c5021
albim72 <marcin.albiniak72@gmail.com>
2023-10-09 10:25:17 +0200

    Create kolekcje.py

---
+++ kolekcje.py
@@ -0,0 +1,22 @@
+print("to jest pierwsza linia")
+print("druga linia")
+print("trzecia linia")
+
+# CTRL+/   - zakomentowanie linii/bloku lub odkomentowanie
+# CTRL + D   - duplikacja linii/ bloku
+# komentarz prosty - kolekcje pythona
+
+"""
+komentarz wieloliniowy - dokumentacyjny
+xyz
+"""
+
+'''
+komentarz wieloliniowy (zwykły)
+abc
+'''
+
+#lista
+
+imiona = ["Jan","Leon","Karol","Olga","Nadia","Leon","Danuta","Maria","Jan"]
+
