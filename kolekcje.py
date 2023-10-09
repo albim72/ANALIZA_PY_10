37a63ff92dc2dbe5d0138e3f8420fd03277d4d6c
albim72 <marcin.albiniak72@gmail.com>
2023-10-09 11:03:32 +0200

    Update kolekcje.py

--- kolekcje.py
+++ kolekcje.py
@@ -32,3 +32,44 @@ print(w[0])
 print(w[1])
 print(w[2:6])
 
+print(imiona[0][2])
+parzyste = imiona[::2]
+print(parzyste)
+
+wyr1 = "listopad"
+wyr2 = "Urszula"
+
+wyr1od = wyr1[::-1]
+wyr2od = wyr2[::-1]
+
+print(wyr1, "-",wyr1od)
+print(wyr2, "-",wyr2od)
+
+imiona.append("Klara")
+print(imiona)
+imiona.insert(3,"Bonifacy")
+print(imiona)
+
+imiona.remove("Maria")
+print(imiona)
+
+imiona.remove("Jan")
+print(imiona)
+
+imiona2 = imiona
+imiona3 = list(imiona)
+imiona4 = imiona[:]
+
+print("_"*40)
+print(type(imiona2))
+print(imiona)
+print(imiona2)
+print(imiona3)
+print(imiona4)
+
+del imiona[2:5]
+print("_"*40)
+print(imiona)
+print(imiona2)
+print(imiona3)
+print(imiona4)
